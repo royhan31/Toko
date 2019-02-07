f92a41404d09f5d03947f7c2c6c9abc9f209eb60
royhan31 <izzapark34@gmail.com>
2019-02-07 19:11:02 +0700

    Auth Admin

---
+++ app/Http/Controllers/AuthAdmin/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\AuthAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    public function showLoginForm(){
+      return view('auth.login');
+    }
+
+    public function login(Request $request){
+      $this->validate($request,[
+        'username' => 'required',
+        'password' => 'required|min:6'
+      ]);
+
+      $credential = [
+        'username' => $request->username,
+        'password' => $request->password
+      ];
+
+      if (Auth::guard('admin')->attempt($credential, $request->member)) {
+        return redirect()->route('home');
+      }
+      // else{
+      //   return redirect()->back()->with('error','Login Gagal')->withInput($request->only('email','remember'));
+      // }
+      return redirect()->back()->withInput($request->only('email','remember'));
+    }
+
+    public function logout(){
+      Auth::guard('admin')->logout();
+      return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Auth::routes();
+
+Route::get('/login','AuthAdmin\LoginController@showLoginForm')->name('login');
+Route::post('/login','AuthAdmin\LoginController@login')->name('admin.login');
+Route::post('/home','AuthAdmin\LoginController@logout')->name('logout');
+Route::get('/home', 'HomeController@index')->name('home');
